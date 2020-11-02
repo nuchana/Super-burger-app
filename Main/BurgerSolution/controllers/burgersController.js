2f8e42bdd88d95d3a025816586feab0a4114845f
Nuchana <nuchana.johnson@gmail.com>
2020-11-01 20:02:53 -0500

    add orm.js and controller

--- Main/BurgerSolution/controllers/burgersController.js
+++ Main/BurgerSolution/controllers/burgersController.js
@@ -1,41 +1,50 @@
 var express = require("express");
 
 var router = express.Router();
-var burger = require("../models/burger.js");
 
-// get route -> index
+// Import the model (cat.js) to use its database functions.
+var burgers = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  res.redirect("/burgers");
-});
-
-router.get("/burgers", function(req, res) {
-  // express callback response by calling burger.selectAllBurger
-  burger.all(function(burgerData) {
-    // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
-    res.render("index", { burger_data: burgerData });
+  burgers.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
   });
 });
 
-// post route -> back to index
-router.post("/burgers/create", function(req, res) {
-  // takes the request object using it as input for burger.addBurger
-  burger.create(req.body.burger_name, function(result) {
-    // wrapper for orm.js that using MySQL insert callback will return a log to console,
-    // render back to index with handle
-    console.log(result);
-    res.redirect("/");
+router.post("/api/burger", function(req, res) {
+  burgers.create([
+    "burger_name", "devoured"
+  ], [
+    req.body.burger_name, req.body.devoured
+  ], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
   });
 });
 
-// put route -> back to index
-router.put("/burgers/:id", function(req, res) {
-  burger.update(req.params.id, function(result) {
-    // wrapper for orm.js that using MySQL update callback will return a log to console,
-    // render back to index with handle
-    console.log(result);
-    // Send back response and let page reload from .then in Ajax
-    res.sendStatus(200);
+router.put("/api/burger/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burgers.update({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
   });
 });
 
+
+
+// Export routes for server.js to use.
 module.exports = router;
--- Main/BurgerSolution/models/burger.js
+++ Main/BurgerSolution/models/burger.js
@@ -1,24 +1,29 @@
+// Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
-var burger = {
+var cat = {
   all: function(cb) {
-    orm.all("burgers", function(res) {
+    orm.all("cats", function(res) {
       cb(res);
     });
   },
-  create: function(name, cb) {
-    orm.create("burgers", [
-      "burger_name", "devoured"
-    ], [
-      name, false
-    ], cb);
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create("cats", cols, vals, function(res) {
+      cb(res);
+    });
   },
-  update: function(id, cb) {
-    var condition = "id=" + id;
-    orm.update("burgers", {
-      devoured: true
-    }, condition, cb);
+  update: function(objColVals, condition, cb) {
+    orm.update("cats", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    orm.delete("cats", condition, function(res) {
+      cb(res);
+    });
   }
 };
 
-module.exports = burger;
+// Export the database functions for the controller (catsController.js).
+module.exports = cat;
