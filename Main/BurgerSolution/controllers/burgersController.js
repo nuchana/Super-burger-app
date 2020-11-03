143ed0a536ee9c3e47ba1512d220d36440cca823
Nuchana <nuchana.johnson@gmail.com>
2020-11-02 23:43:26 -0500

    fix devoured update

--- Main/BurgerSolution/controllers/burgersController.js
+++ Main/BurgerSolution/controllers/burgersController.js
@@ -35,19 +35,13 @@ router.post("/burgers/create", function(req, res) {
 });
 
 router.put("/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+ 
 
-  console.log("condition", condition);
+  burgers.update(req.params.id, function(result) {
+    console.log(result);
 
-  burgers.update({
-    devoured: req.body.devoured
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
+    res.status(200).end();
+   
   });
 });
 
--- Main/BurgerSolution/models/burger.js
+++ Main/BurgerSolution/models/burger.js
@@ -2,23 +2,26 @@
 var orm = require("../config/orm.js");
 
 var burgers = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
+  all: function (cb) {
+    orm.all("burgers", function (res) {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
+  create: function (cols, vals, cb) {
+    orm.create("burgers", cols, vals, function (res) {
       cb(res);
     });
   },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
-  },
-  
+  // update: function(objColVals, condition, cb) {
+  update: function (id, cb) {
+    var condition = "id=" + id;
+    orm.update("burgers", {
+      devoured: true
+    }, condition, cb);
+  }
+
+
 };
 
 // Export the database functions for the controller (catsController.js).
